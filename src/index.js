75bf0f6d814f198301da47d3836429c399666c42
Alex <hungryhex@gmail.com>
2020-08-21 18:19:23 +1000

    Added exit and party functions, added open package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
 const colors = require("colors");
+const open = require("open");
 
 colors.setTheme({
   silly: "rainbow",
@@ -13,19 +14,38 @@ colors.setTheme({
   warn: "yellow",
   debug: "blue",
   error: "red",
+  hello: "cyan",
 });
+
+function partyTime() {
+  console.log("🎩: LET'S GET THIS PAAARRRTTTYYYY STARTED!".silly);
+  open("https://www.youtube.com/watch?v=9osbpEHvQVE");
+  console.log("🎩: WAAHHHHOOOOOO!!".silly);
+  console.log("You passed out...".error);
+  exitProgram();
+}
+
+function exitProgram() {
+  console.log("🎩: Very well sir. Farewell!".warn);
+  process.exit();
+}
 
 inquirer
   .prompt([
     {
       type: "list",
-      message: colors.cyan("🎩: Welcome, what can I do for you sir?"),
+      message: "🎩: Welcome, what can I do for you sir?".hello,
       name: "opening",
-      choices: ["Start a new project", "Just came to say hi..."],
+      choices: ["Start a new project", "PAARRRTY!", "Just came to say hi..."],
     },
   ])
   .then((answers) => {
-    answers.opening;
+    if (answers.opening === "Start a new project") {
+    } else if (answers.opening === "PAARRRTY!") {
+      partyTime();
+    } else {
+      exitProgram();
+    }
   })
   .catch((error) => {
     if (error.isTtyError) {
