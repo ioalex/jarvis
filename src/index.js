11fd1e15efc8ee8faa71ce427dedbbcbb7d20d9d
Alex <hungryhex@gmail.com>
2020-08-21 22:48:40 +1000

    Added Inquirer prompts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+// TODO: Debug prompts!
+
 const inquirer = require("inquirer");
 const colors = require("colors");
 const open = require("open");
@@ -17,40 +20,145 @@ colors.setTheme({
   hello: "cyan",
 });
 
-function partyTime() {
+function partyHandler() {
   console.log("🎩: LET'S GET THIS PAAARRRTTTYYYY STARTED!".silly);
   open("https://www.youtube.com/watch?v=9osbpEHvQVE");
   console.log("🎩: WAAHHHHOOOOOO!!".silly);
+  console.log("Glug glug glug glug...".warn);
   console.log("You passed out...".error);
-  exitProgram();
+  exitHandler();
 }
 
-function exitProgram() {
+function exitHandler() {
   console.log("🎩: Very well sir. Farewell!".warn);
   process.exit();
 }
 
+function errorHandler() {
+  console.log("🎩: Something went wrong...".error);
+  console.log("🎩: Try calling for me again".error);
+  process.exit();
+}
+
+// Initial prompt
 inquirer
   .prompt([
     {
       type: "list",
-      message: "🎩: Welcome, what can I do for you sir?".hello,
       name: "opening",
+      message: "🎩: Welcome, what can I do for you sir?".hello,
       choices: ["Start a new project", "PAARRRTY!", "Just came to say hi..."],
     },
   ])
-  .then((answers) => {
-    if (answers.opening === "Start a new project") {
-    } else if (answers.opening === "PAARRRTY!") {
-      partyTime();
+  .then((opening) => {
+    if (opening === "Start a new project") {
+      startNewProject();
+    } else if (opening === "PAARRRTY!") {
+      partyHandler();
     } else {
-      exitProgram();
+      exitHandler();
     }
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
+      errorHandler();
     }
   });
+
+// Second Prompt
+function startNewProject() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "🎩: Very well sir! What will be the name of your project?"
+          .info,
+      },
+    ])
+    .then((projectName) => {
+      const name = projectName.projectName;
+      console.log(`Ahh ${name}, that's a great name!`);
+      setProjectDirectory();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        errorHandler();
+      }
+    });
+}
+
+// Third Prompt
+// function setProjectDirectory() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         name: "projectDirectory",
+//         message: "🎩: Now, where do you want to save your project?".info,
+//       },
+//     ])
+//     .then((projectDirectory) => {
+//       const name = projectDirectory.projectDirectory;
+//       console.log(`Ahh ${name}, that's a great name!`);
+//     })
+//     .catch((error) => {
+//       if (error.isTtyError) {
+//         errorHandler();
+//       }
+//     });
+// }
+
+// Fourth Prompt
+// function whatFramework() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "confirm",
+//         name: "framework",
+//         message: "🎩: Very well sir! Are you using a framework?".info,
+//       },
+//     ])
+//     .then((answers) => {
+//       if ((answers.framework = "No")) {
+//         selectTools();
+//       } else {
+//         console.log("🎩: Sorry sire, this function has been written yet.".warn);
+//         exitHandler();
+//       }
+//     })
+//     .catch((error) => {
+//       if (error.isTtyError) {
+//         errorHandler();
+//       }
+//     });
+// }
+
+// Fifth Prompt
+// function selectTools() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "checkbox",
+//         name: "tools",
+//         message: "🎩: What will you be using in your project?".info,
+//         choices: [
+//           new inquirer.Separator("--- Formatters & Linters ---"),
+//           "Prettier",
+//           "ESLint",
+//           new inquirer.Separator("--- CSS Preprocessors ---"),
+//           "SCSS",
+//           new inquirer.Separator("--- Build Tools ---"),
+//           "Gulp",
+//           new inquirer.Separator("--- Templating Engines ---"),
+//           "Nunjucks",
+//         ],
+//       },
+//     ])
+//     .then((answers) => {})
+//     .catch((error) => {
+//       if (error.isTtyError) {
+//         errorHandler();
+//       }
+//     });
+// }
